37de66c58711ea02b97ff5bcf0f2cadd667ca047
Emirhan Sartik <sartikemirhan@gmail.com>
2026-08-28 15:34:06 +0300

    Add Migrations

---
+++ Project2_ApiWeather/Context/WeatherContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project2_ApiWeather.Entities;
+
+namespace Project2_ApiWeather.Context
+{
+    public class WeatherContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=EMIRHAN\\SQLEXPRESS;initial catalog=Db2Project8;integrated Security=true;TrustServerCertificate=True");
+        }
+        public DbSet<City> Cities { get; set; }
+    }
+}
