a68bd2679b2c126bc12f39a4f93bc7b617ceb3b4
Emirhan Sartik <sartikemirhan@gmail.com>
2026-08-28 11:53:07 +0300

    Initialize API Weather project and add City entity

---
+++ Project2_ApiWeather/Entities/City.cs
@@ -0,0 +1,11 @@
+﻿namespace Project2_ApiWeather.Entities
+{
+    public class City
+    {
+        public int CityId { get; set; }
+        public string CityName { get; set; }
+        public string Country { get; set; }
+        public decimal Temp { get; set; }
+        public string Detail { get; set; }
+    }
+}
