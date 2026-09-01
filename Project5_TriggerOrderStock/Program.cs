2cc77e0f05ac79a0e0777f58e991304d82c61e70
Emirhan Sartik <sartikemirhan@gmail.com>
2026-09-01 14:42:36 +0300

    Initialize Project5_TriggerOrderStock and setup console UI menu

---
+++ Project5_TriggerOrderStock/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project5_TriggerOrderStock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string number;
+            Console.WriteLine("### Sipariş Stok Sistemi");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("1-Ürün Listesi");
+            Console.WriteLine("2-Sipariş Listesi");
+            Console.WriteLine("3-Kasa Durumu");
+            Console.WriteLine("4-Yeni Ürün Satışı");
+            Console.WriteLine("5-Ürün Stok Güncelleme");
+            Console.WriteLine();
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine();
+
+
+
+            Console.Write("Lütfen Yapmak İstediğiniz İşlemi Seçin: ");
+            number = Console.ReadLine();
+
+            if (number == "1") 
+            {
+                
+            }
+
+        }
+    }
+}
