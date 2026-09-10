56361a667eb7956df379e17a3486d782bff42a5d
Emirhan Sartik <sartikemirhan@gmail.com>
2026-09-10 13:00:10 +0300

    Update MailRegister UI and install Entity Framework for Kaggle dataset

--- Project7_MailRegister/Form1.Designer.cs
+++ Project7_MailRegister/Form1.Designer.cs
@@ -63,7 +63,7 @@
             // 
             this.btnRegister.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.btnRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.btnRegister.Location = new System.Drawing.Point(287, 337);
+            this.btnRegister.Location = new System.Drawing.Point(324, 331);
             this.btnRegister.Name = "btnRegister";
             this.btnRegister.Size = new System.Drawing.Size(241, 56);
             this.btnRegister.TabIndex = 2;
